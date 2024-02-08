860f126bd016e0a308b7bfc7445cd321f6c1a66a
ewdiana <dianapan@gmail.com>
2024-02-08 01:44:25 -0500

    upserts

--- routes/authors.js
+++ routes/authors.js
@@ -7,14 +7,21 @@ router.get('/', function(req, res, next) {
   res.render('authors/index', { title: 'BookedIn || Authors', authors: authors });
 });
 
+
 router.get('/form', async (req, res, next) => {
   res.render('authors/form', { title: 'BookedIn || Authors' });
 });
 
-router.post('/create', async (req, res, next) => {
-  console.log('body: ' + JSON.stringify(req.body))
-  Author.add(req.body);
-  res.redirect(303, '/authors')
+router.post('/upsert', async (req, res, next) => {
+  console.log('body: ' + JSON.stringify(req.body));
+  Author.upsert(req.body);
+  res.redirect(303, '/authors');
+});
+
+router.get('/edit', async (req, res, next) => {
+  let authorIndex = req.query.id;
+  let author = Author.get(authorIndex);
+  res.render('authors/form', { title: 'BookedIn || Authors', author: author, authorIndex: authorIndex });
 });
 
 
